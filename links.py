07ed10578171215f6dedbccb62d7d337787edcc3
ijkoh <mrapire@gmail.com>
2024-10-16 19:30:17 +0300

    add

---
+++ links.py
@@ -0,0 +1,35 @@
+from aiogram.utils.markdown import hlink
+
+hello_url = hlink("Привет, я твой личный помошник", "https://youtu.be/dQw4w9WgXcQ")
+
+# newpcomers
+dev_url = hlink("Страничка новичка - разработчика БАРС Груп",
+                "https://conf.bars.group/pages/viewpage.action?pageId=183236087")
+an_url = hlink("Страничка новичка - аналитка БАРС Груп",
+               "https://conf.bars.group/pages/viewpage.action?pageId=198277483")
+newpie_utl = hlink("Страничка новичка БАРС Груп", "https://conf.bars.group/pages/viewpage.action?pageId=121640225")
+newjkx_url = hlink("Страничка новичка БЦ ЖКХ,СЗ и СТРК","https://conf.bars.group/pages/viewpage.action?pageId=183236064")
+
+templates_url = hlink("ссылке", "https://conf.bars.group/pages/viewpage.action?pageId=56479927")
+references_url = hlink("ссылке", "https://jira.bars.group/servicedesk/customer/portal/13/group/101")
+# vacation etc.
+sickleave_url = hlink("больничный",
+                      "https://conf.bars.group/pages/viewpage.action?pageId=183237343&preview=/183237343/230693376/%D0%98%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%86%D0%B8%D1%8F%20%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D1%87%D0%BD%D1%8B%D0%B9.docx")
+vacation_url = hlink("отпуск",
+                     "https://conf.bars.group/pages/viewpage.action?pageId=183237343&preview=/183237343/213452260/%D0%A1%D0%BE%D0%B3%D0%BB%D0%B0%D1%81%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5%20%D0%BE%D1%82%D0%BF%D1%83%D1%81%D0%BA%D0%B0.docx")
+
+# bonus
+dms_url = hlink("ссылке", "https://conf.bars.group/pages/viewpage.action?pageId=167862245")
+referalprogram_url = hlink("тут", "https://conf.bars.group/pages/viewpage.action?pageId=167851490")
+code_url = hlink("скидок", "https://t.me/barskodbarsgroup")
+biblio_url = hlink("месте", "https://t.me/barsbiblioteka")
+
+# services
+pass_url = hlink("тут", "https://jira.bars.group/servicedesk/customer/portal/8/group/49")
+corporateUn_url = hlink("здесь", "https://conf.bars.group/display/UCT")
+personnel_url = hlink("Отдел Кадров", "https://jira.bars.group/servicedesk/customer/portal/13/group/101")
+techsupport_url = hlink("Техническая поддержка", "https://jira.bars.group/servicedesk/customer/portal/11")
+accounting_url = hlink("бухгалетрию", "https://jira.bars.group/servicedesk/customer/portal/13/group/103")
+
+ideas_url = hlink("сюда", "https://t.me/Suggest_an_idea")
+
