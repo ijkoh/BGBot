40b2365c06c04f8c3972a1e4647fff1e4b5e8417
ijkoh <95040888+ijkoh@users.noreply.github.com>
2024-10-17 18:44:55 +0300

    Update links.py

--- links.py
+++ links.py
@@ -1,17 +1,18 @@
 from aiogram.utils.markdown import hlink
 
-hello_url = hlink("1Привет, я твой личный помошник", "https://youtu.be/dQw4w9WgXcQ")
+#hello_url = hlink("Привет, я твой личный помошник", "https://youtu.be/dQw4w9WgXcQ")
 
-# newpcomers
+# Страничка новичка 
 dev_url = hlink("Страничка новичка - разработчика БАРС Груп",
                 "https://conf.bars.group/pages/viewpage.action?pageId=183236087")
-an_url = hlink("Страничка новичка - аналитка БАРС Груп",
-               "https://conf.bars.group/pages/viewpage.action?pageId=198277483")
-newpie_utl = hlink("Страничка новичка БАРС Груп", "https://conf.bars.group/pages/viewpage.action?pageId=121640225")
+an_url = hlink("ссылка на битрикс - страницу по направлению")
+
+newpie_utl = hlink("ссылка на битрикс - страницу по направлению")
 newjkx_url = hlink("Страничка новичка БЦ ЖКХ,СЗ и СТРК","https://conf.bars.group/pages/viewpage.action?pageId=183236064")
 
 templates_url = hlink("ссылке", "https://conf.bars.group/pages/viewpage.action?pageId=56479927")
 references_url = hlink("ссылке", "https://jira.bars.group/servicedesk/customer/portal/13/group/101")
+
 # vacation etc.
 sickleave_url = hlink("больничный",
                       "https://conf.bars.group/pages/viewpage.action?pageId=183237343&preview=/183237343/230693376/%D0%98%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%86%D0%B8%D1%8F%20%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D1%87%D0%BD%D1%8B%D0%B9.docx")
