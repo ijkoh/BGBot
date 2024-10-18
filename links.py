6f0657087b116da039a00589f184753d82f891f3
ijkoh <95040888+ijkoh@users.noreply.github.com>
2024-10-18 18:22:44 +0300

    Update links.py

--- links.py
+++ links.py
@@ -15,13 +15,13 @@ references_url = hlink("ссылке", "https://bx.crg.im/stream/")
 
 
 ## Отпуск/Больничный/Отгул
-sickleave_url = hlink("больничный",
+# sickleave_url = hlink("больничный",
                       "https://conf.bars.group/pages/viewpage.action?pageId=183237343&preview=/183237343/230693376/%D0%98%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%86%D0%B8%D1%8F%20%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D1%87%D0%BD%D1%8B%D0%B9.docx")
-vacation_url = hlink("отпуск",
+# vacation_url = hlink("отпуск",
                      "https://conf.bars.group/pages/viewpage.action?pageId=183237343&preview=/183237343/213452260/%D0%A1%D0%BE%D0%B3%D0%BB%D0%B0%D1%81%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5%20%D0%BE%D1%82%D0%BF%D1%83%D1%81%D0%BA%D0%B0.docx")
 
-#sickleave_url = hlink("больничный", "https://bx.crg.im/stream/")
-#vacation_url = hlink("отпуск", "https://bx.crg.im/stream/")
+sickleave_url = hlink("больничный", "https://bx.crg.im/stream/")
+vacation_url = hlink("отпуск", "https://bx.crg.im/stream/")
 #otgyl_url = hlink("больничный", "https://bx.crg.im/stream/")
 
 # bonus
